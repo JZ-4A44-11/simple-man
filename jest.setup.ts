f7b36bf6af93a652f965e10fd191b982e5ffb34b
Joseph Estevez <joseph.estevez@protonmail.com>
2022-05-02 12:49:19 -0400

    fix ts-jest integration
    Downgrade jest to version 27 by fix ts-jest integration,
    reinterpreted the env file path.

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,5 +2,5 @@ import { join } from 'path'
 import { config } from 'dotenv'
 
 module.exports = async (): Promise<void> => {
-  config({ path: join(process.cwd(), 'test/.env.testing') })
+  config({ path: join(__dirname, '.env.testing') })
 }
